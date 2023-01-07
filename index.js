2bd4db9fa0ec74a4afd2832f02545c1ba2ac21c8
Rajnikant Panchal <rajnikant916@gmail.com>
2023-01-07 14:05:51 +0530

    added excel file code

--- index.js
+++ index.js
@@ -1,30 +1,92 @@
-const XLSX = require('xlsx')
+const ExcelJS = require("exceljs");
+const fs = require("fs");
 
 // Require express and body-parser
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 // Initialize express and define a port
-const app = express()
-const PORT = 5000
+const app = express();
+const PORT = 5000;
 // Tell express to use body-parser's JSON parsing
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.get("/download/report", (req, res) => {
+  const filePath = "export.xlsx";
+  res.download(filePath, "export.xlsx", (err) => {
+    if (err) {
+      res.send({
+        error: err,
+        msg: "Problem downloading the file",
+      });
+    }
+  });
+});
+
 app.post("/hook", (req, res) => {
-    generateData(req.body);
-  res.status(200).end() // Responding is important
-})
+  generateData(req.body);
+  res.status(200).end(); // Responding is important
+});
 
+const generateData = async (res) => {
+  console.log(JSON.stringify(res)); // Call your action on the request here
 
-const generateData = (res) => {
+  try {
+    let jsonData = res;
+    if (jsonData && jsonData.resource && jsonData.step && jsonData.step.data) {
+      const verificationId = jsonData.resource.substring(
+        jsonData.resource.lastIndexOf("/") + 1
+      );
+      data = [
+        {
+          "Verification ID": verificationId,
+          IdentityStatus: jsonData.step.data.fullName,
+          Name: jsonData.step.data.fullName,
+          DocumentNumber: jsonData.step.data.dateOfBirth,
+        },
+      ];
 
-  console.log(res);
-  data = [{}]
-   
-   //const ws = XLSX.utils.json_to_sheet(data)
-   //const wb = XLSX.utils.book_new()
-   //XLSX.utils.book_append_sheet(wb, ws, 'Responses')
-   //XLSX.writeFile(wb, 'sampleData.xlsx')
-
-}
+      let path = "export.xlsx";
+      const workbook = new ExcelJS.Workbook();
+      let worksheet = null;
+      if (fs.existsSync(path)) {
+        await workbook.xlsx.readFile(path);
+        worksheet = workbook.getWorksheet("Responses");
+      } else {
+        worksheet = workbook.addWorksheet("Responses");
+      }
+      worksheet.columns = [
+        { header: "Verification ID", key: "VerificationID", width: 10 },
+        { header: "IdentityStatus", key: "IdentityStatus", width: 32 },
+        { header: "Name.", key: "Name", width: 15 },
+        { header: "DocumentNumber", key: "DocumentNumber", width: 32 },
+        { header: "Resource", key: "resource", width: 32 },
+      ];
+      worksheet.addRow({
+        VerificationID: verificationId,
+        IdentityStatus: "",
+        Name: jsonData.step.data.fullName,
+        DocumentNumber: jsonData.step.data.documentNumber,
+        resource: jsonData.resource,
+      });
+      await workbook.xlsx.writeFile(path);
+    } else if (jsonData && jsonData.resource && jsonData.identityStatus) {
+      let path = "export.xlsx";
+      const workbook = new ExcelJS.Workbook();
+      if (fs.existsSync(path)) {
+        workbook.xlsx.readFile(path).then(function () {
+          var worksheet = workbook.getWorksheet("Responses");
+          var lastRow = worksheet.lastRow;
+          lastRow.getCell(2).value = jsonData.identityStatus;
+          lastRow.commit();
+          return workbook.xlsx.writeFile(path);
+        });
+      }
+    }
+  } catch (err) {
+    console.log("Not able to parse the Response " + res);
+    console.log("Error " + err);
+  }
+};
 
 // Start express on the defined port
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
